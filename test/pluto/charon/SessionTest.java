65c1d63811059b9264bc12d131849c89e00cb912
georgeradev <georgeradev@f3f1db35-e174-4b56-a93b-e342b4b1320d>
2015-10-20 14:49:25 +0000

    adding core functions for db

--- test/pluto/charon/SessionTest.java
+++ test/pluto/charon/SessionTest.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.sql.Connection;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 import junit.framework.TestCase;
 import org.sqlite.JDBC;
@@ -28,6 +29,7 @@ public class SessionTest extends TestCase {
 				private final JDBC driver = new JDBC();
 				private final Properties driverParameters = new Properties();
 				private final String connectionString;
+
 				{
 					connectionString = properties.getProperty(DBManager.DB_JDBC);
 				}
@@ -47,45 +49,84 @@ public class SessionTest extends TestCase {
 		sessionManager.start();
 
 		Charon client = new Charon("localhost", port, 0);
-		client.ping();
-		client.login("test", "test");
-		client.ping();
-		client.ping();
-		client.login("newUser", "test");
-		client.ping();
+		{ // connection
+			client.ping();
+			client.login("test", "test");
+			client.ping();
+			client.ping();
+			client.login("newUser", "test");
+			client.ping();
 
-		String expect;
-		client.plutoSet("id", expect = "value");
-		String value = client.plutoGet("id");
-		assertEquals(expect, value);
-		client.ping();
+			String expect;
+			client.plutoSet("id", expect = "value");
+			String value = client.plutoGet("id");
+			assertEquals(expect, value);
+			client.ping();
+		}
 
-		// local
-		assertEquals(2L, client.charonExecute("1+1;"));
+		{// local
+			assertEquals(2L, client.charonExecute("1+1;"));
 
-		// execute
-		String result = client.plutoExecute("1+1;");
-		assertEquals("2", result);
+			// execute
+			Object result = client.plutoExecute("1+1;");
+			assertEquals("2", result);
 
-		// source
-		result = client.plutoExecute("function func(a,b){return (a) + (b);}");
-		assertEquals("function func(a,b){return (a) + (b);}", result);
+			// source
+			String source = "function func(a,b){return (a) + (b);}";
+			result = client.plutoExecute(source);
+			assertTrue(result instanceof List);
+			assertEquals(source, ((List)result).get(0).toString());
 
-		// call
-		result = client.plutoCall("'test substring'.substring", 4, 8);
-		assertEquals(" sub", result);
+			// call
+			result = client.plutoCall("'test substring'.substring", 4, 8);
+			assertEquals(" sub", result);
 
-		// frame
-		result = client.plutoExecute("new {a:2,b:'str'};");
-		assertEquals("{\na : 2,\nb : \"str\"\n}", result);
+			// frame
+			result = client.plutoExecute("new {a:2,b:'str'};");
+			assertTrue(result instanceof Map);
+			assertEquals("2", ((Map)result).get("a").toString());
+			assertEquals("str", ((Map)result).get("b").toString());
 
-		// search
-		client.plutoSet("id1", "value1");
-		client.plutoSet("id2", "value2");
-		List<String> search = client.plutoSearch("id", 2);
-		assertEquals(2, search.size());
-		search = client.plutoSearch("", -1);
-		assertTrue(search.size() > 2);
+			// search
+			client.plutoSet("id1", "value1");
+			client.plutoSet("id2", "value2");
+			List<String> search = client.plutoSearch("id", 2);
+			assertEquals(2, search.size());
+			search = client.plutoSearch("", -1);
+			assertTrue(search.size() > 2);
+		}
+		
+		{// import
+			client.plutoSet("object1", "{a:1,b:'string'}");
+			Object obj = client.charonExecute("obj = import('object1');");
+			assertTrue(obj instanceof Map);
+			String str = String.valueOf(client.charonExecute("obj.b;"));
+			assertEquals("string", str);
+		}
+
+		{// include
+			client.plutoSet("object1", "{a:1,b:'string'}");
+			Object obj = client.charonExecute("obj = include('object1');");
+			assertTrue(obj instanceof Map);
+			String str = String.valueOf(client.charonExecute("obj.b;"));
+			assertEquals("string", str);
+		}
+
+		{// import
+			client.plutoSet("object1", "{a:1,b:'string'}");
+			Object obj = client.plutoExecute("obj = import('object1');");
+			assertTrue(obj instanceof Map);
+			String str = String.valueOf(client.plutoExecute("obj.b;"));
+			assertEquals("string", str);
+		}
+
+		{// include
+			client.plutoSet("object1", "{a:1,b:'string'}");
+			Object obj = client.plutoExecute("obj = include('object1');");
+			assertTrue(obj instanceof Map);
+			String str = String.valueOf(client.plutoExecute("obj.b;"));
+			assertEquals("string", str);
+		}
 
 		client.logout();
 		client.close();
